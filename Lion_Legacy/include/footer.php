33680edabec5197f53fdb2a8a8e6ae81e65bd0f5
prakash751 <prakashreddy95155@gmail.com>
2024-06-16 13:50:58 -1200

    first commit

---
+++ Lion_Legacy/include/footer.php
@@ -0,0 +1,6 @@
+<footer>
+			<img src="images/lion logo 2.jpg " width="40px" height="40px">
+			<br>
+			<br>
+			Copyright Lion Corp.
+</footer>
---
+++ Lion_Legacy/include/header.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<!--jquery-->
+	<script type="text/javascript" src="../jquery/jquery-3.6.0.min.js"></script>
+	<!--bootstrap css-->
+	<link rel="stylesheet" type="text/css" href="../bootstrap 3.3/css/bootstrap.css">
+	<!--bootstrap js-->
+	<script type="text/javascript" src="../bootstrap 3.3/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/lion.css">
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>LION STRENGTHS</title>
+</head>
+<body class="bgwhite">
+	<!--header-->
+	<nav class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<a href="#" class=" navbar-brand">
+					<img height="100%" width="100%" src="images/lion logo 4.jpg">
+			    </a>
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="navbar-collapse collapse" id="mynavbar">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="homepage.html">Home</a></li>
+					<li class=""><a href="about.html">About</a></li>
+					<li class=""><a href="strength.html">Strengths</a></li>
+					<li class=""><a href="whylion.html">WhyLion</a></li>
+					<li class=""><a href="merchandise.html">Lion Attitude</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!--header-->
+		<div class="container">
+			<div class="col-md-12">
+			<div class="thumbnail">
+				<img src="../sample pics/lion 1.jpg" class="img-responsive img-rounded">
+				<div class="caption">
+					<h1>KING OF JUNGLE</h1>
+					<p>The fearless mighty lion is the king of the jungle</p>
+				</div>
+			</div>	
+		</div>
+	</div>
+	<!--header-->
